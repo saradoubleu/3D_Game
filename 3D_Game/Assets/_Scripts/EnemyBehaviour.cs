47fe79f641d0e17d5c3932b70b5ec768456f0cd7
saradoubleu <sara.in.3d@gmail.com>
2016-11-20 13:31:59 -0500

    enemy behaviour

---
+++ 3D_Game/Assets/_Scripts/EnemyBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+    //PRIVATE INSTANCE VARIABLES
+    private Transform Target;
+
+    //PUBLIC INSTANCE VARIABLES
+    public NavMeshAgent Agent;
+
+        // Use this for initialization
+	void Start () {
+        this.Target = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this.Agent.SetDestination(this.Target.position);
+
+	}
+}
